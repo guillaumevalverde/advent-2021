0d6a04d3581977c87b92f253d2f077ba6d07348e
Gve <guillaume.valverde@gmail.com>
2021-12-07 09:07:27 +0100

    week1

--- src/main/kotlin/week1/GenerateParenthesisCombinations.kt
+++ src/main/kotlin/week1/GenerateParenthesisCombinations.kt
@@ -1,4 +1,44 @@
 package week1
 
-fun generateParenthesisCombinations(num: Int): List<String> = TODO()
+import java.lang.Integer.max
 
+fun generateParenthesisCombinations(num: Int): List<String> =
+    generate(emptyList(), Generator("", 0, num, num))
+
+data class Generator(val acc: String, val stillOpened: Int, val open: Int, val close: Int)
+
+fun close(generator: Generator): Generator =
+    generator.copy(
+        stillOpened = max(0, generator.stillOpened - 1),
+        close = max(0, generator.close - 1),
+        acc = generator.acc + ")"
+    )
+
+fun open(generator: Generator): Generator =
+    generator.copy(
+        stillOpened = generator.stillOpened + 1,
+        open = max(0, generator.open - 1),
+        acc = generator.acc + "("
+    )
+
+private tailrec fun generate(accList: List<String>, generator: Generator): List<String> {
+    return if (generator.open <= 0 && generator.close <= 0) {
+        accList + listOf(generator.acc)
+    } else {
+        if (generator.stillOpened == 0 && generator.open > 0) {
+            generate(
+                accList,
+                open(generator)
+            )
+        } else if (generator.stillOpened > 0 && generator.open > 0 && generator.close > 0) {
+            generate(
+                generate(accList, close(generator)),
+                open(generator)
+            )
+        } else if (generator.stillOpened > 0 && generator.open <= 0 && generator.close > 0) {
+            generate(accList, close(generator))
+        } else {
+            accList
+        }
+    }
+}
--- src/main/kotlin/week1/JsonStringify.kt
+++ src/main/kotlin/week1/JsonStringify.kt
@@ -14,5 +14,22 @@ data class JsonString(val value: String) : JsonElement()
 data class JsonBoolean(val value: Boolean) : JsonElement()
 object JsonNull : JsonElement()
 
-fun JsonElement.stringify(): String = TODO()
+private fun String.addSeparator(): String = if (this.isEmpty()) "" else "$this,"
 
+private fun JsonArray.stringifyArray() = this.elements.fold("") { acc, ell -> "${acc.addSeparator()}${ell.stringify()}" }
+
+private fun List<Pair<String, JsonElement>>.stringifyList() =
+    this.fold("") { acc, ell -> "${acc.addSeparator()}\"${ell.first}\":${ell.second.stringify()}"}
+
+private fun Double.stringifyDouble() : String =
+    if ((this.toInt() - this) == 0.0  ) this.toInt().toString() else this.toString()
+
+fun JsonElement.stringify(): String =
+    when(this) {
+        is JsonArray -> "[${this.stringifyArray()}]"
+        is JsonBoolean -> if (this.value) "true" else "false"
+        JsonNull -> "null"
+        is JsonNumber -> this.value.stringifyDouble()
+        is JsonObject -> "{${this.fields.toList().stringifyList()}}"
+        is JsonString -> "\"${this.value}\""
+    }
